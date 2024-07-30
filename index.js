cc9868da2b2bc4bca544939bca93555f92e4d31f
brian <brianagatundu@gmail.com>
2024-07-30 15:25:56 +0300

    add error handlers

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { connectDatabase } = require("./dataBase/connectDb");
+const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 dotenv.config(); // load .env variables
 
 const MONGOURL = process.env.MONGOURL;
 const PORT = process.env.PORT;
+
+app.use(errorHandler);
 
 const startServer = async () => {
   await connectDatabase(MONGOURL);
---
+++ middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (error, req, res, next) => {
+  if (error instanceof CustomError) {
+    return res
+      .status(error.status)
+      .json({ status: error.status, message: error.message });
+  }
+
+  res.status(500).json("Internal server error");
+};
+
+module.exports = { errorHandler };
